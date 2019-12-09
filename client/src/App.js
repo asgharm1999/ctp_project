7509e3f1b1a968fbf0f8b3d78506467bb7c08b7e
asgharm1999 <asgharm1999@gmail.com>
2019-12-09 12:20:22 -0500

    added chat features

--- client/src/App.js
+++ client/src/App.js
@@ -13,61 +13,175 @@ import { Layout } from './components/Layout';
 import { NavigationBar } from './components/NavigationBar';
 import { Jumbotron } from './components/Jumbotron';
 import './App.css';
+import 'skeleton-css/css/normalize.css';
+import 'skeleton-css/css/skeleton.css';
+import './App.css';
+import {
+  handleInput,
+  connectToChatkit,
+  connectToRoom,
+  sendMessage,
+  sendDM,
+} from './methods';
+import Dialog from './components/Dialog';
+import RoomList from './components/RoomList';
+import ChatSession from './components/ChatSession';
+import RoomUsers from './components/RoomUsers';
 
-
-
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
+class App extends Component {
+  constructor() {
+    super();
     this.state = {
-      isUserLoggedIn: false,
-      userLat: null,
-      userLong: null,
-      userName: ''
-    }
-  }
+      userId: '',
+      showLogin: true,
+      isLoading: false,
+      currentUser: null,
+      currentRoom: null,
+      rooms: [],
+      roomUsers: [],
+      roomName: null,
+      messages: [],
+      newMessage: '',
+    };
 
- //Finds if user is logged in | passed down to login component: App.js -> Home.js -> Login.js
-  getUserStatus(status) {
-      this.setState({
-        isUserLoggedIn: status
-      }) 
-  }
 
-  getUserName(name) {
-      this.setState({
-        userName: name
-      }) 
+    this.handleInput = handleInput.bind(this);
+    this.connectToChatkit = connectToChatkit.bind(this);
+    this.connectToChatkit = connectToChatkit.bind(this);
+    this.connectToRoom = connectToRoom.bind(this);
+    this.sendMessage = sendMessage.bind(this);
+    this.sendDM = sendDM.bind(this);
   }
-
-  getUserLocation(location) {
-      this.setState({
-        userLat: location.coords.latitude,
-        userLong: location.coords.longitude
-      }) 
-  }
-
 
   render() {
+    const {
+      userId,
+      showLogin,
+      rooms,
+      currentRoom,
+      currentUser,
+      messages,
+      newMessage,
+      roomUsers,
+      roomName,
+    } = this.state;
 
-    const userLocation = this.state;
+    return (
+      <div className="App">
+        <aside className="sidebar left-sidebar">
+          {currentUser ? (
+            <div className="user-profile">
+              <span className="username">{currentUser.name}</span>
+              <span className="user-id">{`@${currentUser.id}`}</span>
+            </div>
+          ) : null}
 
-    return(
-      <React.Fragment>
-      <NavigationBar />
-      <MapComponent 
-        userstatus={this.state.isUserLoggedIn}  
-        getUserLocation={(e) => this.getUserLocation(e)} />
-      <Login 
-        getUserStatus={(e) => this.getUserStatus(e)} />
-      <Jumbotron />
-      <Layout>
-        <Home />
-        <Signup userlat={this.state.userLat} userlong={this.state.userLong}/>
-        <UserBio getUserStatus={(e) => this.getUserStatus(e)}/>
-      </Layout>
-      <Footer />
-      </React.Fragment>
-      );
+          {currentRoom ? (
+            <RoomList
+              rooms={rooms}
+              currentRoom={currentRoom}
+              connectToRoom={this.connectToRoom}
+              currentUser={currentUser}
+            />
+          ) : null}
+        </aside>
+        <section className="chat-screen">
+          <header className="chat-header">
+            {currentRoom ? <h3>{roomName}</h3> : null}
+          </header>
+          <ul className="chat-messages">
+            <ChatSession messages={messages} />
+          </ul>
+          <footer className="chat-footer">
+            <form onSubmit={this.sendMessage} className="message-form">
+              <input
+                type="text"
+                value={newMessage}
+                name="newMessage"
+                className="message-input"
+                placeholder="Type your message and hit ENTER to send"
+                onChange={this.handleInput}
+              />
+            </form>
+          </footer>
+        </section>
+        <aside className="sidebar right-sidebar">
+          {showLogin ? (
+            <Dialog
+              userId={userId}
+              handleInput={this.handleInput}
+              connectToChatkit={this.connectToChatkit}
+            />
+          ) : null}
+
+          {currentRoom ? (
+            <RoomUsers
+              currentUser={currentUser}
+              sendDM={this.sendDM}
+              roomUsers={roomUsers}
+            />
+          ) : null}
+        </aside>
+      </div>
+    );
   }
 }
+
+export default App;
+
+
+// export default class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       isUserLoggedIn: false,
+//       userLat: null,
+//       userLong: null,
+//       userName: ''
+//     }
+//   }
+
+//  //Finds if user is logged in | passed down to login component: App.js -> Home.js -> Login.js
+//   getUserStatus(status) {
+//       this.setState({
+//         isUserLoggedIn: status
+//       }) 
+//   }
+
+//   getUserName(name) {
+//       this.setState({
+//         userName: name
+//       }) 
+//   }
+
+//   getUserLocation(location) {
+//       this.setState({
+//         userLat: location.coords.latitude,
+//         userLong: location.coords.longitude
+//       }) 
+//   }
+
+
+//   render() {
+
+//     const userLocation = this.state;
+
+//     return(
+//       <React.Fragment>
+//       <NavigationBar />
+//       <MapComponent 
+//         userstatus={this.state.isUserLoggedIn}  
+//         getUserLocation={(e) => this.getUserLocation(e)} />
+//       <Login 
+//         getUserStatus={(e) => this.getUserStatus(e)} />
+//       <Jumbotron />
+//       <Layout>
+//         <Home />
+//         <Signup userlat={this.state.userLat} userlong={this.state.userLong}/>
+//         <UserBio getUserStatus={(e) => this.getUserStatus(e)}/>
+//       </Layout>
+//       <Footer />
+//       </React.Fragment>
+//       );
+//   }
+// }
